d742b315a8e1ebd8c81edf43ce43eab95013a3e8
njamb94 <njamb94@hotmail.com>
2017-05-28 22:19:15 +0200

    Dodat layout za DetailsPage.

---
+++ app/src/main/java/com/njamb/geodrink/DetailsPage.java
@@ -0,0 +1,19 @@
+package com.njamb.geodrink;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class DetailsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
