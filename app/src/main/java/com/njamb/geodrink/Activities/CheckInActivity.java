6498c9ba3b841edd6328af44389f5d583a6436e1
Stefan Golubovic <golub1994@gmail.com>
2017-06-10 11:32:33 +0200

    Update(CheckInActivity): take picture with camera

--- app/src/main/java/com/njamb/geodrink/Activities/CheckInActivity.java
+++ app/src/main/java/com/njamb/geodrink/Activities/CheckInActivity.java
@@ -1,12 +1,21 @@
 package com.njamb.geodrink.Activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.njamb.geodrink.R;
 
 public class CheckInActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,30 @@ public class CheckInActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imageView = (ImageView) findViewById(R.id.checkin_iv_photo);
 
+        Button addPhoto = (Button) findViewById(R.id.checkin_btn_addphoto);
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageView.setImageBitmap(imageBitmap);
+            }
+        }
+    }
 }
