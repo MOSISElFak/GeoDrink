cbc7df5e4b2055454d4f83291568fc0f34717770
Stefan Golubovic <golub1994@gmail.com>
2017-07-19 14:21:33 +0200

    Add validator to ResetPasswordActivity

--- app/src/main/java/com/njamb/geodrink/authentication/ResetPasswordActivity.java
+++ app/src/main/java/com/njamb/geodrink/authentication/ResetPasswordActivity.java
@@ -13,11 +13,21 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.njamb.geodrink.R;
 
-public class ResetPasswordActivity extends AppCompatActivity {
+import java.util.List;
+
+public class ResetPasswordActivity extends AppCompatActivity implements Validator.ValidationListener {
 
     private ProgressDialog pd;
+
+    @NotEmpty
+    @Email private EditText email;
+    private Validator mValidator = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,41 +40,60 @@ public class ResetPasswordActivity extends AppCompatActivity {
         pd.setIndeterminate(true);
         pd.setCancelable(false);
 
+        mValidator = new Validator(this);
+        mValidator.setValidationListener(this);
+
+        email = (EditText) findViewById(R.id.edit_rst_email);
+
         Button btnSend = (Button) findViewById(R.id.btn_send_rst_pass);
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText etMail = (EditText) findViewById(R.id.edit_rst_email);
-                String email = etMail.getText().toString();
-                // TODO: email validation
-                if (email.trim().equals("")) {
-                    Toast.makeText(ResetPasswordActivity.this,
-                            "Please enter your mail", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-                    pd.show();
-                    FirebaseAuth.getInstance()
-                            .sendPasswordResetEmail(email)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-                                    pd.dismiss();
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(ResetPasswordActivity.this,
-                                                "Email sent", Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    }
-                                    else {
-                                        Toast.makeText(ResetPasswordActivity.this,
-                                                "Sending email failed. Try again.",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                }
+                mValidator.validate();
             }
         });
+    }
+
+    private void sendResetPasswordEmail() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        pd.show();
+        FirebaseAuth.getInstance()
+                .sendPasswordResetEmail(email.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                        pd.dismiss();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ResetPasswordActivity.this,
+                                    "Email sent", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(ResetPasswordActivity.this,
+                                    "Sending email failed. Try again.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        sendResetPasswordEmail();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            }
+            else {
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
